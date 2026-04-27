6ecb6a708d9731b458d010de4b184235edc731bd
NormieCodes <rishisagaram5@gmail.com>
2026-04-27 12:03:42 +0530

    add search based filtering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
 import { Routes, Route } from 'react-router';
+import { useState } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import Home from '../pages/Home.jsx';
 import Cart from '../pages/Cart.jsx';
+import Contact from '../components/Contact.jsx';
 import '../style/App.css';
 import CartBar from '../components/CartBar.jsx';
 function App() {
+  const [search, setSearch] = useState("");
   return (
     <>
-    <Navbar />
+    <Navbar setSearch={setSearch} />
     <div className="animate-background"></div>
 
     <Routes> 
-      <Route path='/' element={<Home />} />
-      <Route path='Cart' element={<Cart />} />
+      <Route path='/' element={<Home search={search} />} />
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/contact' element={<Contact />} />
     </Routes>
       <CartBar />
     </>
